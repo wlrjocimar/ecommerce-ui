97f4e4c3f4954d9b43d54af5669b8e57cb8b2477
Jocimar Walter <jocimarwalter@bb.com.br>
2025-10-30 21:20:18 -0300

    updated

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,18 @@ console.log(`Loaded environment: ${appEnv} (${envFile})`);
 const nextConfig: NextConfig = {
   reactStrictMode: true,
    basePath:'/ecommerce-ui',
-   assetPrefix: '/ecommerce-ui/' // 👈 adiciona prefixo para assets
+   images: {
+    // Configurações específicas para imagens
+   // domains: ['exemplo.com'], // se usar imagens externas
+    path: '/ecommerce-ui/_next/image', // importante com basePath
+    // unoptimized: true, // Mantenha false para otimização
+    // Configurações específicas para PNG
+    formats: ['image/webp', 'image/avif'],
+  },
+  // Importante para produção
+  trailingSlash: false, // ou true, mas seja consistente
+  assetPrefix:'/ecommerce-ui',
+  //  assetPrefix: '/ecommerce-ui/' // 👈 adiciona prefixo para assets
   // env: {
   //   NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   // },
